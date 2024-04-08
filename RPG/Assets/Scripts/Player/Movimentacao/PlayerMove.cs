aae89c2215546d18fcda8ff179e35ef664d627cd
Kuro <iaguinhodosantos2@gmail.com>
2024-04-08 19:19:56 -0300

    🏃‍♂️ Indo ali correr
    Animação de correr juntamente com animação de ataque melee.

--- RPG/Assets/Scripts/Player/Movimentacao/PlayerMove.cs
+++ RPG/Assets/Scripts/Player/Movimentacao/PlayerMove.cs
@@ -4,7 +4,9 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float speedPlayer;
     private CharacterController characterPlayer;
-    private Animator animationPlayer;    
+    private Animator animationPlayer;
+    private int layerIndex = 1;
+    private float layerHeight;
     private Vector3 movementPlayer;
 
     void Awake()
@@ -23,8 +25,31 @@ public class PlayerMove : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        movementPlayer = (transform.right * horizontal) + (transform.forward * vertical);
+
+        movementPlayer = (transform.right * horizontal) + (transform.forward * vertical);      
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animationPlayer.SetBool("Correr", true);
+            movementPlayer = (transform.right * horizontal) + (transform.forward * vertical) * 2;
+        }
+        else
+        {
+            animationPlayer.SetBool("Correr", false);
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            animationPlayer.SetBool("Ataque", true);         
+            layerHeight = 1f;
+            animationPlayer.SetLayerWeight(layerIndex, layerHeight);
+        }
+        else
+        {
+            animationPlayer.SetBool("Ataque", false);
+            layerHeight = 0f;
+            animationPlayer.SetLayerWeight(layerIndex, layerHeight);
+        }
         characterPlayer.Move(movementPlayer.normalized * speedPlayer * Time.deltaTime);
+
         PlayerAnimation(horizontal, vertical);
     }
 
