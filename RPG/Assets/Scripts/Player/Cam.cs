1da84ce12d7958839660846f32754424169a6e43
Henri Lopes <henrilopes16@gmail.com>
2024-04-16 07:28:53 -0300

    Mais Camera
    Não está mais torta

--- RPG/Assets/Scripts/Player/Cam.cs
+++ RPG/Assets/Scripts/Player/Cam.cs
@@ -34,23 +34,26 @@ public class Cam : MonoBehaviour
     }
     void FollowTarget()
     {
-        Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref cameraVelocity,cameraSmoothSpeed * Time.deltaTime);
+        Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
         transform.position = targetCameraPosition;
 
     }
     void Rotations()
     {
         leftAndRightLookAngle += (player.mouseX * leftAndRightRotationSpeed) * Time.deltaTime;
-        upAndDownLookAngle += (player.mouseY * upAndDownRotationSpeed) * Time.deltaTime;
+        upAndDownLookAngle -= (player.mouseY * upAndDownRotationSpeed) * Time.deltaTime;
         upAndDownLookAngle = Mathf.Clamp(upAndDownLookAngle, minimunPivot, maximunPivot);
+
         Vector3 cameraRotation = Vector3.zero;
+        Quaternion targetRotation;
+
         cameraRotation.y = leftAndRightLookAngle;
-        Quaternion targetRotation = Quaternion.Euler(cameraRotation);
+        targetRotation = Quaternion.Euler(cameraRotation);
         transform.rotation = targetRotation;
 
         cameraRotation = Vector3.zero;
         cameraRotation.x = upAndDownLookAngle;
-        transform.rotation = Quaternion.Euler(cameraRotation);
+        targetRotation = Quaternion.Euler(cameraRotation);
         cameraPivotTransform.localRotation = targetRotation;
 
     }
