d3334665b3c4aace13d06e90da726266077c16ec
Kuro <iaguinhodosantos2@gmail.com>
2024-04-18 17:53:17 -0300

    😏 Tudo isso e mais um pouco
    Câmera funcionando junto com nova animação/movimentação.
    Rolamento ainda com probleminhas.
    No one knows what it's like
    To be the bad man
    To be the sad man
    Behind blue eyes

---
+++ RPG/Assets/Scripts/Player/Movimento2/CameraHandler.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    public Transform targetTransform;
+    public Transform cameraTransform;
+    public Transform CameraPivotTransform;
+    private Transform myTransform;
+    private Vector3 cameraTransformPosition;
+    private LayerMask ignoreLayers;
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+
+    public static CameraHandler singleton;
+
+    public float lookSpeed = 0.1f;
+    public float followSpeed = 0.1f;
+    public float pivotSpeed = 0.03f;
+
+    private float targetPosition;
+    private float defaultPosition;
+    private float lookAngle;
+    private float pivotAngle;
+    public float minimumPivot = -35;
+    public float maximumPivot = 35;
+
+    public float cameraSphereRadius = 0.2f;
+    public float cameraCollisionOffset = 0.2f;
+    public float minimumCollisionOffset = 0.2f;
+
+    private void Awake()
+    {
+        singleton = this;
+        myTransform = transform;
+        defaultPosition = cameraTransform.localPosition.z;
+        ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
+    }
+
+    public void FollowTarget(float delta)
+    {
+        Vector3 targerPosition = Vector3.SmoothDamp(myTransform.position, targetTransform.position, ref cameraFollowVelocity, delta / followSpeed);
+        myTransform.position = targerPosition;
+
+        HandleCameraCollision(delta);
+    }
+
+    public void HandleCameraRotation(float delta, float mouseXInput, float mouseYInput)
+    {
+        lookAngle += (mouseXInput * lookSpeed) / delta;
+        pivotAngle -= (mouseYInput * pivotAngle) / delta;
+        pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = lookAngle;
+        Quaternion targerRotation = Quaternion.Euler(rotation);
+        myTransform.rotation = targerRotation;
+
+        rotation = Vector3.zero;
+        rotation.x = pivotAngle;
+
+        targerRotation = Quaternion.Euler(rotation);
+        CameraPivotTransform.localRotation = targerRotation;
+    }
+
+    private void HandleCameraCollision(float delta)
+    {
+        targetPosition = defaultPosition;
+        RaycastHit hit;
+        Vector3 direciton = cameraTransform.position - CameraPivotTransform.position;
+        direciton.Normalize();
+        if(Physics.SphereCast(CameraPivotTransform.position, cameraSphereRadius, direciton, out hit, Mathf.Abs(targetPosition), ignoreLayers))
+        {
+            float dis = Vector3.Distance(CameraPivotTransform.position, hit.point);
+            targetPosition = -(dis - cameraCollisionOffset);
+        }
+        if(Mathf.Abs(targetPosition) < minimumCollisionOffset)
+        {
+            targetPosition = -minimumCollisionOffset;
+        }
+        cameraTransformPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, delta / 0.2f);
+        cameraTransform.localPosition = cameraTransformPosition;
+    }
+}
---
+++ RPG/Assets/Scripts/Player/Movimento2/PlayerMoviment.cs
@@ -0,0 +1,132 @@
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    Transform cameraObject;
+    InputHandler inputHandler;
+    Vector3 moveDirection;
+
+    [HideInInspector]
+    public Transform myTransform;
+    [HideInInspector]
+    public AnimatorHandle animatorHandler;
+
+    public new Rigidbody rigidbody;
+    public GameObject normalCamera;
+
+    [Header("Stats")]
+    [SerializeField]
+    float movementSpeed = 5;
+    [SerializeField]
+    float sprintSpeed;
+    [SerializeField]
+    float rotationSpeed = 10;
+
+    public bool isSprinting;
+
+
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        inputHandler = GetComponent<InputHandler>();
+        animatorHandler = GetComponentInChildren<AnimatorHandle>();
+        cameraObject = Camera.main.transform;
+        myTransform = transform;
+        animatorHandler.Initialize();
+    }
+
+    public void Update()
+    {
+        float delta = Time.deltaTime;
+
+        isSprinting = inputHandler.b_Input;
+        inputHandler.TickInput(delta);
+        HandleMovement(delta);
+        HandleRollingAndSprinting(delta);
+    }
+
+    #region Movement
+    Vector3 normalVector;
+    Vector3 targetPosition;
+
+    private void HandleRotation(float delta)
+    {
+        Vector3 targetDir = Vector3.zero;
+        float moveOverride = inputHandler.moveAmout;
+
+        targetDir = cameraObject.forward * inputHandler.vertical;
+        targetDir += cameraObject.right * inputHandler.horizontal;
+
+        targetDir.Normalize();
+        targetDir.y = 0;
+
+        if (targetDir == Vector3.zero)
+            targetDir = myTransform.forward;
+
+        float rs = rotationSpeed;
+
+        Quaternion tr = Quaternion.LookRotation(targetDir);
+        Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+        myTransform.rotation = targetRotation;
+    }
+
+    public void HandleMovement(float delta)
+    {
+        if (inputHandler.rollFlag)
+            return;
+
+        moveDirection = cameraObject.forward * inputHandler.vertical;
+        moveDirection += cameraObject.right * inputHandler.horizontal;
+        moveDirection.Normalize();
+
+        float speed = movementSpeed;
+
+        if (inputHandler.sprintFlag)
+        {
+            speed = sprintSpeed;
+            isSprinting = true;
+            moveDirection *= speed;
+        }
+        else
+        {
+            moveDirection *= speed;
+
+        }
+
+        Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+        rigidbody.velocity = projectedVelocity;
+
+        animatorHandler.UpdateAnimatorValues(inputHandler.moveAmout, 0, isSprinting);
+
+        if (animatorHandler.canRotate)
+        {
+            HandleRotation(delta);
+        }
+    }
+
+    public void HandleRollingAndSprinting(float delta)
+    {
+        if (animatorHandler.anim.GetBool("isInteracting"))
+            return;
+        if (inputHandler.rollFlag)
+        {
+            moveDirection = cameraObject.forward * inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+
+            if(inputHandler.moveAmout > 0)
+            {
+                animatorHandler.PlayTargetAnimation("Rolling", true);
+                moveDirection.y = 0;
+                Quaternion rollRotation = Quaternion.LookRotation(moveDirection);
+                myTransform.rotation = rollRotation;
+            }
+            else
+            {
+                animatorHandler.PlayTargetAnimation("Backstep", true);
+            }
+        }
+    }
+    #endregion
+}
