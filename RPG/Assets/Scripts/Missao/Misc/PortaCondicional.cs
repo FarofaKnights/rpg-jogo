67b2769d227b5206e94e58d2f7dcb7de7483c385
Juan Vitor <juanriven10@gmail.com>
2024-06-16 19:17:48 -0300

    🚪 Portas
    Arrumei a cena do henri pra ter as portas funcionando com keycards que não são removiveis e deletei os varios keycards um em cima do outro.
    
    Também adicionei uma opção para consumir o item em uma porta condicional de item (vem desabilitada por padrão)

--- RPG/Assets/Scripts/Missao/Misc/PortaCondicional.cs
+++ RPG/Assets/Scripts/Missao/Misc/PortaCondicional.cs
@@ -10,11 +10,23 @@ public class PortaCondicional : MonoBehaviour {
 
     Condicao condicao;
 
+    public bool consumirItem = false;
+
     void Start() {
         condicao = condicaoInfo.GetCondicao();
         condicao.Then(() => {
+            if (consumirItem && condicao.GetType() == typeof(CondicaoTemItem)) {
+                CondicaoTemItem cond = (CondicaoTemItem)condicao;
+                Player.instance.inventario.RemoveItem(cond.itemData, cond.quantidade);
+            }
+
             Destroy(porta);
         });
+
+        if (GetComponent<Interagivel>() != null) {
+            Interagivel inte = GetComponent<Interagivel>();
+            if (inte.tagFilter == null) inte.tagFilter = "Player";
+        }
     }
 
     public void OnPlayerInteract() {
