5c1b400166f72e47f01b0c9adf2d052b4916271f
Claudio Ramos <claudio.ramos@sga.pucminas.br>
2024-06-29 09:08:50 -0300

    Fornalha testes
    Ainda não funcionando 100%, porem eu não quero ter o risco do meu pc morrer e perder todo o progresso

---
+++ RPG/Assets/Scripts/Skills/ProjetilExplosivo.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjetilExplosivo : MonoBehaviour
+{
+    public float velocidade = 10;
+    public float tempoDeVida = 2;
+    public float dano = 1;
+    public GameObject Explosao;
+
+    public string tagAlvo = "Player";
+
+    bool alreadyHit = false;
+
+    void Start()
+    {
+        Destroy(gameObject, tempoDeVida);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * velocidade * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tagAlvo))
+        {
+            if (alreadyHit) return;
+            alreadyHit = true;
+
+            PossuiVida vida = other.GetComponent<PossuiVida>();
+            if (vida != null)
+            {
+                vida.LevarDano(dano);
+            }
+            Destroy(gameObject);
+        }
+        else
+        {
+            Explosao.SetActive(true);
+        }
+
+    }
+}
