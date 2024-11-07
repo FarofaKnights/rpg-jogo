5620c28544d590dff572121b8e7207bd20837969
Guilherme D. <guilherme.dias69420@gmail.com>
2024-11-06 21:03:18 -0300

    Neve ❄️
    Um shader complicado de neve com tesselation e umas coisas aí. É pra dar pra fazer pegadas com ele, mas tive uns problemas, então por enquanto fica sem

---
+++ RPG/Assets/Scripts/SetInteractiveShaderEffect.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SetInteractiveShaderEffects : MonoBehaviour
+{
+    [SerializeField]
+    RenderTexture rt;
+    [SerializeField]
+    Transform target;
+    void Awake()
+    {
+        Shader.SetGlobalTexture("_GlobalEffectRT", rt);
+        Shader.SetGlobalFloat("_OrthographicCamSize", GetComponent<Camera>().orthographicSize);
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        Shader.SetGlobalVector("_Position", transform.position);
+    }
+
+
+}
