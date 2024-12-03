4ded684fcb645bb747c63c0c2e363a05469b1108
Juan Vitor <juanriven10@gmail.com>
2024-12-03 20:51:56 -0300

    🪛 Resolvendo problemas
    Missões dando erro (não era pra dar) e agora o abismo tem como tirar o objetivo é nunca mais existir esse script eu odeio tanto ele pqp

--- RPG/Assets/Scripts/Temp/Abismo.cs
+++ RPG/Assets/Scripts/Temp/Abismo.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Abismo : MonoBehaviour
-{
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Coisa");
+public class Abismo : MonoBehaviour {
+    void Awake() {
+        Debug.Log("Substituir pelo script correto, tenho como objetivo excluir o script Abismo até o final da build, conto com a colaboração de todos! Clique nos ... do componente e vai ter a opção 'Setup Correct Script', só clicar nela :)");
+        SetupCorrectScript();
+    }
 
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Playyeyey");
-            Destroy(other.gameObject);
-            GameManager.instance.GameOver();
+    [ContextMenu("Setup Correct Script")]
+    public void SetupCorrectScriptMenu() {
+        SetupCorrectScript(true);
+    }
+    
+    public void SetupCorrectScript(bool imediato = false) {
+        OnTrigger onT = gameObject.GetComponent<OnTrigger>();
+        if (onT == null) onT = gameObject.AddComponent<OnTrigger>();
+
+        FallTrigger fallT = gameObject.GetComponent<FallTrigger>();
+        if (fallT == null) {
+            fallT = gameObject.AddComponent<FallTrigger>();
+            fallT.contarComoMorte = true;
         }
-       
+
+        if (gameObject.layer != LayerMask.NameToLayer("Trigger")) {
+            gameObject.layer = LayerMask.NameToLayer("Trigger");
+        }
+        
+        if (imediato) DestroyImmediate(this);
+        else Destroy(this);
     }
 }
